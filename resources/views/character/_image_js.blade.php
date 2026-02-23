cfbafe8e52a672da90bdb77dd10718e8a6f9bb7e
min <liwoyadans@gmail.com>
2026-02-22 18:46:41 -0800

    fix(themes): tinymce stuff

--- resources/views/character/_image_js.blade.php
+++ resources/views/character/_image_js.blade.php
@@ -19,18 +19,18 @@
                     ],
                     toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | code',
                     content_css: [
-                        '{{ asset('css/app.css') }}',
-                        '{{ asset('css/lorekeeper.css?v=' . filemtime(public_path('css/lorekeeper.css'))) }}',
-                        '{{ asset('css/custom.css') }}',
-                        '{{ asset($theme?->cssUrl) }}',
-                        '{{ asset($conditionalTheme?->cssUrl) }}',
-                        '{{ asset($decoratorTheme?->cssUrl) }}',
-                        '{{ asset('css/all.min.css') }}' //fontawesome
+                        '{{ asset("css/app.css") }}',
+                        '{{ asset("css/lorekeeper.css?v=" . filemtime(public_path("css/lorekeeper.css"))) }}',
+                        '{{ asset("css/all.min.css") }}',
+                        {!! file_exists(public_path() . "/css/custom.css") ? "'" . asset("css/custom.css?v=") . filemtime(public_path("css/custom.css")) . "'," : "" !!}
+                        {!! $theme?->cssUrl ? "'" . asset($theme?->cssUrl) . "'," : "" !!}
+                        {!! $conditionalTheme?->cssUrl ? "'" . asset($conditionalTheme?->cssUrl) . "'," : "" !!}
+                        {!! $decoratorTheme?->cssUrl ? "'" . asset($decoratorTheme?->cssUrl) . "'," : "" !!} 
                     ],
                     content_style: `
-                    {{ str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $theme])) }}
-                    {{ str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $conditionalTheme])) }}
-                    {{ str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $decoratorTheme])) }}
+                        {!! str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $theme])) !!}
+                        {!! isset($conditionalTheme) && $conditionalTheme ? str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $conditionalTheme])) : '' !!}
+                        {!! isset($decoratorTheme) && $decoratorTheme ? str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $decoratorTheme])) : '' !!}
                     `,
                     spoiler_caption: 'Toggle Spoiler',
                     target_list: false
@@ -73,12 +73,13 @@
                     ],
                     toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | code',
                     content_css: [
-                        '{{ asset('css/app.css') }}',
-                        '{{ asset('css/lorekeeper.css?v=' . filemtime(public_path('css/lorekeeper.css'))) }}',
-                        {!! file_exists(public_path() . '/css/custom.css') ? "'" . asset('css/custom.css?v=') . filemtime(public_path('css/custom.css')) . "'," : '' !!}
-                        {!! $theme?->cssUrl ? "'" . asset($theme?->cssUrl) . "'," : '' !!}
-                        {!! $conditionalTheme?->cssUrl ? "'" . asset($conditionalTheme?->cssUrl) . "'," : '' !!}
-                        {!! $decoratorTheme?->cssUrl ? "'" . asset($decoratorTheme?->cssUrl) . "'," : '' !!} '{{ asset('css/all.min.css') }}' //fontawesome
+                        '{{ asset("css/app.css") }}',
+                        '{{ asset("css/lorekeeper.css?v=" . filemtime(public_path("css/lorekeeper.css"))) }}',
+                        '{{ asset("css/all.min.css") }}',
+                        {!! file_exists(public_path() . "/css/custom.css") ? "'" . asset("css/custom.css?v=") . filemtime(public_path("css/custom.css")) . "'," : "" !!}
+                        {!! $theme?->cssUrl ? "'" . asset($theme?->cssUrl) . "'," : "" !!}
+                        {!! $conditionalTheme?->cssUrl ? "'" . asset($conditionalTheme?->cssUrl) . "'," : "" !!}
+                        {!! $decoratorTheme?->cssUrl ? "'" . asset($decoratorTheme?->cssUrl) . "'," : "" !!} 
                     ],
                     content_style: `
                         {!! str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $theme])) !!}
