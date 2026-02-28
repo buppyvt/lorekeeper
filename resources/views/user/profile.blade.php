484daa3fac11ce6eb7da2006f4684dd0ce68a586
pastel-nebula-admin <ssconpp@yahoo.com>
2026-02-27 22:11:03 -0600

    no message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -17,18 +17,7 @@
 </h1>
 <div class="mb-1">
     <div class="row">
-        <div class="row col-md-6">
-            <div class="col-md-4 col-4"><h5>Alias</h5></div>
-            <div class="col-md-8 col-8">{!! $user->displayAlias !!}</div>
-        </div>
-        <div class="row col-md-6">
-            <div class="col-md-4 col-4"><h5>Joined</h5></div>
-            <div class="col-md-8 col-8">{!! format_date($user->created_at, false) !!} ({{ $user->created_at->diffForHumans() }})</div>
-        </div>
-        <div class="row col-md-6">
-            <div class="col-md-4 col-4"><h5>Rank</h5></div>
-            <div class="col-md-8 col-8">{!! $user->rank->displayName !!} {!! add_help($user->rank->parsed_description) !!}</div>
-        </div>
+        
         @if($user->birthdayDisplay && isset($user->birthday))
             <div class="row col-md-6">
                 <div class="col-md-4 col-4"><h5>Birthday</h5></div>
