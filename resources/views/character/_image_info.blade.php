c34f427805c449e3161d1f8aebb94ec317b7f5bd
pastel-nebula-admin <ssconpp@yahoo.com>
2026-06-05 18:59:55 -0500

    no message

--- resources/views/character/_image_info.blade.php
+++ resources/views/character/_image_info.blade.php
@@ -59,20 +59,20 @@
                     </div>
                  @if($image->subtype_id)
                     <div class="row">
-                        <div class="col-lg-4 col-md-6 col-4"><h5>Subtype</h5></div>
-                        <div class="col-lg-8 col-md-6 col-8">{!! $image->subtype_id ? $image->subtype->displayName : 'None' !!}</div>
+                        <div class="col-lg-4 col-4"><h5>Subtype</h5></div>
+                        <div class="col-lg-8 col-8">{!! $image->subtype_id ? $image->subtype->displayName : 'None' !!}</div>
                     </div>
                 @endif
                 @if($image->character->homeSetting)
                     <div class="row">
-                        <div class="col-lg-4 col-md-6 col-4"><h5>Home</h5></div>
-                        <div class="col-lg-8 col-md-6 col-8">{!! $image->character->location ? $image->character->location : 'None' !!}</div>
+                        <div class="col-lg-4 col-4"><h5>Home</h5></div>
+                        <div class="col-lg-8 col-8">{!! $image->character->location ? $image->character->location : 'None' !!}</div>
                     </div>
                 @endif
                 @if($image->character->factionSetting)
                     <div class="row">
-                        <div class="col-lg-4 col-md-6 col-4"><h5>Faction</h5></div>
-                        <div class="col-lg-8 col-md-6 col-8">{!! $image->character->faction ? $image->character->currentFaction : 'None' !!}{!! $character->factionRank ? ' ('.$character->factionRank->name.')' : null !!}</div>
+                        <div class="col-lg-4 col-4"><h5>Faction</h5></div>
+                        <div class="col-lg-8 col-8">{!! $image->character->faction ? $image->character->currentFaction : 'None' !!}{!! $character->factionRank ? ' ('.$character->factionRank->name.')' : null !!}</div>
                     </div>
                 @endif
                 <div class="row no-gutters">
